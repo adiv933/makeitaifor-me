1b2454aef524b86b4a32119f29bd9ec0be35302a
clumsydog09 <adityav090304@gmail.com>
2024-05-17 12:35:48 +0530

    Added mdx dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-}
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you want to use MDXComponents from a custom file
+    // provider you can pass the path to that file here:  
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
+module.exports = withMDX({
+  ...nextConfig,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+});
